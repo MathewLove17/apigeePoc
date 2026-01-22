2ac6b43c1a522585f395ac4b91a6c9fdaa718589
Matías Amor <matipinto2@gmail.com>
2026-01-22 12:24:14 +0100

    ingress

--- terraform/runtime/infra/main.tf
+++ terraform/runtime/infra/main.tf
@@ -35,10 +35,14 @@ module "vpc" {
   # Recomendado para EKS + Load Balancers
   public_subnet_tags = {
     "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
+
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
+
 }
 
 ####################
