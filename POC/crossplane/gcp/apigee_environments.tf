9af0422a26d8a2c80a109919dbbdb91c23bae898
Matías Amor <matipinto2@gmail.com>
2026-01-29 17:17:40 +0100

    error

--- POC/crossplane/gcp/apigee_environments.tf
+++ POC/crossplane/gcp/apigee_environments.tf
@@ -1,5 +1,5 @@
 resource "google_apigee_environment" "env" {
-  # MUY importante: usar el ID de la org para que el provider forme bien la URL
+  
   org_id = google_apigee_organization.hybrid_org.id
   name   = var.apigee_environment
 }
--- POC/crossplane/gcp/wait_apis.tf
+++ POC/crossplane/gcp/wait_apis.tf
@@ -3,6 +3,5 @@ resource "time_sleep" "wait_for_gcp_services" {
     google_project_service.apigee_apis
   ]
 
-  # Ajusta si tu org tarda más: "120s"
   create_duration = "300s"
 }
